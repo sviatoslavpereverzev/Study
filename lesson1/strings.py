d8cf97d1a505b3cea483d538a8b5c1a70cdca504
Sviatoslav Pereverziev <>
2022-04-16 12:50:03 +0300

    First commit

---
+++ lesson1/strings.py
@@ -0,0 +1,25 @@
+name = 'Alona'
+age = 26
+
+
+# # комментарий над кодом
+# print('Hello name. Age: age')  # тестовая строка
+# print('Hello ' + name + '. Age: ' + str(age))  # вариант 1
+# print('Hello {}. Age: {}'.format(name, age))
+# print('Hello %s. Age: %s' % (name, age))
+# print(f'Hello {name}. Age: {age}')
+
+
+def add(a, b):
+    return a + b
+
+
+result1 = 2 + 3
+result2 = add(5, 8)
+result = add(result1, result2)
+print(result)
+# add(6, 4)
+# add(10, 3)
+# add(3, 4)
+# add(2, 324)
+# add(3443, 4)
